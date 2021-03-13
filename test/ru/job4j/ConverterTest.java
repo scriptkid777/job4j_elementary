54b66cec4c28781aed0d9c6a400f3ed8c21aa31e
scriptkid777 <44677406+scriptkid777@users.noreply.github.com>
2021-03-13 13:50:38 +0700

    1. Модульные тесты. [#485635]

--- test/ru/job4j/ConverterTest.java
+++ test/ru/job4j/ConverterTest.java
@@ -16,7 +16,7 @@ public class ConverterTest {
     @Test
     public void whenConvert180RblThen3Dlr() {
         int in = 180;
-        int expected = 4;
+        int expected = 3;
         int out = Converter.rubleToDollar(in);
         Assert.assertEquals(expected, out);
     }
