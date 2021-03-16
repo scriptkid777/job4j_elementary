4aedbd64a39ab291f73fe9d2bbe16e67839ae9c2
$(scriptkid777) <$(scriptkid777)@users.noreply.github.com>
2021-03-16 15:29:13 +0700

    3. Тесты для определения расстояния между точками. [#485637]

--- src/ru/job4j/Info.java
+++ src/ru/job4j/Info.java
@@ -2,6 +2,11 @@ package ru.job4j;
 
 public class Info {
     public static void main(String[] args) {
+        short s = 1500;
+        char c = (char) s;
+        double d = c;
+        System.out.println(d);
+
         System.out.println("02.03.2021");
     }
 }
